214b770f3ffc0fdad3a29f8f1d327ecf9159a551
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-05-27 11:00:41 +0300

    updated mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -8,6 +8,7 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
+<<<<<<< HEAD
 def mergesort(array):
     if len(array) <= 1:
         return array
@@ -37,6 +38,8 @@ def merge(left, right):
     return merged
 
 
+=======
+>>>>>>> parent of dd8c56c (Merge-sort)
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
@@ -48,5 +51,9 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
+<<<<<<< HEAD
     sorted_list = mergesort(value_list)
     print(sorted_list)
+=======
+    debug_print(value_list=value_list)
+>>>>>>> parent of dd8c56c (Merge-sort)
